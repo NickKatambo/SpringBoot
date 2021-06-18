3c395c7d4af524836ae05f4a63f5fdf784a92615
Yannick Katambo <yannick.katambo@outlook.com>
2021-06-18 14:08:45 +0200

    Class implementation and mock student data.

--- src/main/java/com/nickkatambo/Dao/StudentDao.java
+++ src/main/java/com/nickkatambo/Dao/StudentDao.java
@@ -1,4 +1,27 @@
 package com.nickkatambo.Dao;
 
+import com.nickkatambo.Entity.Student;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class StudentDao {
+
+    private static Map<Integer, Student> students;
+    static {
+        students = new HashMap<Integer, Student>(){
+            {
+                // To copy lines downward use CTRL + D
+                put(1, new Student(1, "Pharez","Katambo","Medicine"));
+                put(2, new Student(2, "Pearl","Katambo","Electrical Engineer"));
+                put(3, new Student(3, "Johan","Katambo","Computer Science"));
+                put(4, new Student(4, "Nick","Katambo","Lawyer"));
+            }
+        };
+    }
+
+    public Collection<Student> getAllStudents(){
+        return students.values();
+    }
 }
---
+++ src/main/java/com/nickkatambo/Entity/Student.java
@@ -0,0 +1,52 @@
+package com.nickkatambo.Entity;
+
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String course;
+
+    //shortcut for creating a constructor is ALT + INSERT
+
+    public Student(int id, String firstName, String lastName, String course) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.course = course;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
